339830f5921770e2933524a5c6fe52dd982a46b0
aronbs <101139622+aronbs@users.noreply.github.com>
2022-03-12 17:10:34 +0000

    1st assignment done, created a file for the second one

---
+++ bin/assignment4-1.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:core';
+import 'dart:math';
+
+
+void main(List<String> arguments){
+
+//1
+
+  List<int> calculateSum = [13,56,77,3,45,6,944,1,43,7,40,55,67,93,45,754,33];
+  int sumTotal = 0;
+
+  for(int num in calculateSum)
+    {
+      sumTotal += num;
+    }
+  print('The sum of all elements in the list is: $sumTotal');
+
+
+
+
+
+
+}
