1a6a22ad7f8d6c75c6fa79895766685ccac38d36
aronbs <101139622+aronbs@users.noreply.github.com>
2022-03-12 17:35:35 +0000

    finished the second assignment and made the 3rd file

---
+++ bin/assignment4-2.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:core';
+import 'dart:math';
+
+
+void main(List<String> arguments){
+
+  //2
+
+  List<int> calculateSum = [13,56,77,3,45,6,944,1,43,7,40,55,67,93,45,754,33];
+  List<int> newList =[];
+
+
+  for(int num in calculateSum)
+    {
+      if(num >= 20 && num <= 80)
+        {
+          newList.add(num);
+          print(num);
+
+        }
+    }
+  print('');
+  print('There are ${newList.length} numbers that met these conditions');
+
+
+
+
+
+
+
+
+}
---
+++ bin/assignment4-3.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:core';
+import 'dart:math';
+
+
+void main(List<String> arguments){
+
+  //3
+
+
+  
+
+
+
+
+
+
+}
