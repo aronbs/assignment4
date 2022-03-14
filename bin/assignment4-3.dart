f09b6c12d4bcb3f4b541782519c9aebf019d65ea
aronbs <101139622+aronbs@users.noreply.github.com>
2022-03-14 22:52:08 +0000

    improvements

--- bin/assignment4-3.dart
+++ bin/assignment4-3.dart
@@ -14,25 +14,38 @@ void main(List<String> arguments){
 
 
 
-  while(number != '')
+  while(true)
   {
     print('Enter as many numbers as you would like, when you are done press enter');
     number = int.tryParse(stdin.readLineSync());
-    numberList.add(number);
 
     if(number == null)
     {
       break;
     }
+    numberList.add(number);
+
 
   }
-  numberList.removeLast();
-  print('The numbers you entered were: ${numberList.join(',')}');
 
   numberList.sort();
 
-  print('The largest number was: ${numberList.last}');
-  print('The smallest number was: ${numberList.first}');
+  if(numberList.isNotEmpty && numberList.length > 1) {
+    print('The numbers you entered were: ${numberList.join(',')}');
+    print('The largest number was: ${numberList.last}');
+    print('The smallest number was: ${numberList.first}');
+  }
+
+  else if(numberList.isEmpty)
+      {
+        print("You didn't enter any numbers!");
+      }
+
+  else
+    {
+      print('You only entered one number: ${numberList.join('')}');
+    }
+  }
 
 
 
@@ -42,4 +55,3 @@ void main(List<String> arguments){
 
 
 
-}
